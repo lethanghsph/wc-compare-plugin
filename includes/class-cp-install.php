2c329d1274e6aa674535b6c84da8827e249bab88
LeThang <lethanghsph@gmail.com>
2017-03-28 15:33:30 +0700

    class install when activate plugin

---
+++ includes/class-cp-install.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Installation related functions and actions.
+ *
+ * @author   WooThemes
+ * @category Admin
+ * @package  WooCommerce/Classes
+ * @version  2.4.1
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * WC_Install Class.
+ */
+class WC_Install {
+	/**
+	 * Install Compare.
+	 */
+	public function install() {
+
+	}
+}
+
+// WC_Install::init();
